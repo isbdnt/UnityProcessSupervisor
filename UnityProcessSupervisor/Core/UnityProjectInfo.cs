7b2e0df26629c5124f02224a611ce8715dff4bba
isbdnt <isbdnt@outlook.com>
2019-08-30 14:49:45 +0800

    Initial commit

---
+++ UnityProcessSupervisor/Core/UnityProjectInfo.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityProcessSupervisor;
+
+namespace UnityProcessSupervisor {
+    public class UnityProjectInfo {
+        public string projectName;
+        public string projectPath;
+    }
+}
---
+++ UnityProcessSupervisor/View/UnityProcessView.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnityProcessSupervisor.View {
+    public partial class UnityProcessView : UserControl {
+        public event EventHandler<UnityProcessInfo> onRestart = delegate { };
+        public event EventHandler<UnityProcessInfo> onClose = delegate { };
+
+        public UnityProcessView() {
+            InitializeComponent();
+            this.restart.Click += HandleRestartClick;
+            this.close.Click += HandleCloseClick;
+        }
+
+        void HandleRestartClick(object sender, EventArgs e) {
+            onRestart(sender, processInfo);
+        }
+
+        void HandleCloseClick(object sender, EventArgs e) {
+            onClose(sender, processInfo);
+        }
+
+        UnityProcessInfo processInfo;
+
+        public UnityProcessInfo ProcessInfo {
+            get => this.processInfo;
+            set {
+                this.processInfo = value;
+                this.projectName.Text = value.project.projectName;
+                this.projectPath.Text = value.project.projectPath;
+            }
+        }
+    }
+}
